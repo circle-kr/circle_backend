602db4f8d5cee1cf5cf6f7b283fee892a232f5e5
geneeuchoi <chyj2108@naver.com>
2024-12-30 21:44:50 +0900

    init commit

---
+++ src/main/java/com/example/circle_backend/CircleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.circle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/circle_backend/CircleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circle_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
