0c668338853e84bd105c4bd1e1d41725e3b54604
geneeuchoi <chyj2108@naver.com>
2025-01-14 01:03:22 +0900

    refactor: UserCreateDto > UserCreateRequest 클래스 이름 변경

--- src/main/java/com/circle/circle_backend/user/service/UserService.java
+++ src/main/java/com/circle/circle_backend/user/service/UserService.java
@@ -1,7 +1,7 @@
 package com.circle.circle_backend.user.service;
 
 import com.circle.circle_backend.user.domain.User;
-import com.circle.circle_backend.user.domain.UserCreateDto;
+import com.circle.circle_backend.user.domain.UserCreateRequest;
 import jakarta.transaction.Transactional;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +15,8 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public User create(UserCreateDto userCreateDto) {
-        User user = User.from(userCreateDto);
+    public User create(UserCreateRequest userCreateRequest) {
+        User user = User.from(userCreateRequest);
         user = userRepository.save(user);
         return user;
     }
