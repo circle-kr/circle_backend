0abc9e35a247a3df226297a955587c2c5b998b71
geneeuchoi <chyj2108@naver.com>
2025-01-23 19:26:47 +0900

    feat: 서클 가입 상태 정보 가져오기 refactor: findByUserIdAndCircleId 메서드 반환타입 Optional로 변경

--- src/main/java/com/circle/circle_backend/Enrollment/infrastructure/EnrollmentJpaRepository.java
+++ src/main/java/com/circle/circle_backend/Enrollment/infrastructure/EnrollmentJpaRepository.java
@@ -3,8 +3,10 @@ package com.circle.circle_backend.Enrollment.infrastructure;
 import com.circle.circle_backend.Enrollment.infrastructure.entity.EnrollmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface EnrollmentJpaRepository extends JpaRepository<EnrollmentEntity, Long> {
 
-    EnrollmentEntity findByUserIdAndCircleId(Long userId, Long circleId);
+    Optional<EnrollmentEntity> findByUserIdAndCircleId(Long userId, Long circleId);
 }
